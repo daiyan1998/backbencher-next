8d83df9a5d7324f13e94edd45759104e0757ef95
daiyan1998 <abduddaiyan7@gmail.com>
2024-03-09 13:02:03 +0600

    fixed nav and banner

--- src/components/shared/DynamicBanner/DynamicBanner.jsx
+++ src/components/shared/DynamicBanner/DynamicBanner.jsx
@@ -1,6 +1,6 @@
 const DynamicBanner = ({ content }) => {
   return (
-    <div className="flex md:flex-row flex-col justify-between my-[100px] px-0 lg:gap-7">
+    <div className="flex md:flex-row flex-col justify-between md:my-[100px] my-9 px-0 lg:gap-7">
       <div className="md:basis-1/2 leading-loose">{content?.title}</div>
 
       <div className="md:basis-1/2 text-[#000000b3]">{content?.subPara}</div>
